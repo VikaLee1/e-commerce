1653f135ae3fde8fe9b210ee0217ce02e9f14ecb
Nikola Sijan <nikola.sijan992@gmail.com>
2022-12-14 15:02:35 +0100

    nik1

--- project/details.php
+++ project/details.php
@@ -25,17 +25,34 @@ if ($_GET['id']) {
         $size=$data['size'];
         $price=$data['price'];
         $image=$data['image'];
-        
-
+       
       
         
     } else {
         header("location: error.php");
     }
-    
 } else {
     header("location: error.php");
 }
+
+if(isset($_POST['add_to_cart'])){
+
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
+ 
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+ 
+    if(mysqli_num_rows($check_cart_numbers) > 0){
+       $message[] = 'already added to cart!';
+    }else{
+       mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+       $message[] = 'product added to cart!';
+    }
+ 
+ }
+
 ?>
 
 <!DOCTYPE html>
@@ -89,18 +106,32 @@ if ($_GET['id']) {
                         <th>Price:</th>
                         <td><?=$price ?> €</td>
                         </tr>
-                        
-               
                     
-                  
                 </table>
-                
+               
             </form>
+           
             </div>
-      
+ <div style="display: flex;justify-content:center">
+            <form action="" method="post" class="box">
+     
+     <input type="hidden" min="1" name="product_quantity" value="1" class="qty">
+     <input type="hidden" name="product_name" value="<?php echo $name?>">
+     <input type="hidden" name="product_price" value="<?php echo $price ?>">
+     <input type="hidden" name="product_image" value="<?php echo $image?>">
+     <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+     
+    
+    </form>
+    </div>
             
 
+
+
+
+            
         <?php include 'footer.php'; ?>
+       
 
 <!-- custom js file link  -->
 <script src="js/script.js"></script>
